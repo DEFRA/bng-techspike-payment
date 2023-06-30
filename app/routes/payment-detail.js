6f89b73a6356b7f0505985ab4d08e945a46a6197
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-06-30 14:46:49 +0100

    add events

--- app/routes/payment-detail.js
+++ app/routes/payment-detail.js
@@ -1,17 +1,26 @@
-const joi = require('joi')
 const config = require('../config')
-const { get } = require('../api')
+const { get, post } = require('../api')
 
 module.exports = [
-    {
-      method: 'GET',
-      path: '/payment-detail/{id}',
-      options: {
-        handler: async (request, h) => {
-            const res = await get(`${config.paymentApiUrl}/${request.params.id}`, config.paymentApiKey)
-            return h.view('payment-detail', res)
-        },
+  {
+    method: 'GET',
+    path: '/payment-detail/{id}',
+    options: {
+      handler: async (request, h) => {
+        const paymentResponse = await get(`${config.paymentApiUrl}/${request.params.id}`, config.paymentApiKey)
+        const eventsResponse = await get(`${config.paymentApiUrl}/${request.params.id}/events`, config.paymentApiKey)
+        return h.view('payment-detail', { payment: paymentResponse, events: eventsResponse.events })
       }
     }
+  },
+  {
+    method: 'POST',
+    path: '/payment-detail/cancel/{id}',
+    options: {
+      handler: async (request, h) => {
+        await post(`${config.paymentApiUrl}/${request.params.id}/cancel`, {}, config.paymentApiKey)
+        return h.redirect('/view-payments')
+      }
+    }
+  }
 ]
-  
